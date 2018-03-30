debafc5e4fb1548da7caa95c57829446cb8d2da7
tushalien <agrawaltushar63@gmail.com>
2018-03-30 11:28:49 +0530

    Added app folder

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
+
+$( document ).ready(function()
+ {
+  if (typeof web3 !== 'undefined') {
+    console.warn("Using web3 detected from external source like Metamask")
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+});
--- truffle.js
+++ truffle.js
@@ -1,4 +1,28 @@
+require('babel-register')
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "This is conifedential . :P"
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/GT650Dz4fUm2eSgLUfX8")
+      },
+      network_id: 3,
+      gas: 4600000
+    },
+  	development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 4600000// Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/GT650Dz4fUm2eSgLUfX8")
+      },
+      network_id: 4,
+      gas: 4600000
+    }   
+  }
 };
