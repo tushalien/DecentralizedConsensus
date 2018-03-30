c4a15bfb5320a7b22c37965c37ae59f4c8930c25
tushalien <agrawaltushar63@gmail.com>
2018-03-30 11:27:26 +0530

    Added Webpack file

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './app/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './app/index.html', to: "index.html" }
+          ])
+  ],
+  module: {
+    rules: [
+      {
+          test: /\.(jpe?g|png|gif|svg|woff|woff2|eot|ttf)$/i,  // a regular expression that catches .js files
+          exclude: /node_modules/,
+          loader: 'url-loader',
+      }    
+    ,
+      {
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+    loaders: [
+      { test: /\.json$/, use: 'json-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  }
+}
