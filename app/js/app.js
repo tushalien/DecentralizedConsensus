d59640a1ecd9549c1874a99c314b40ec0d077024
tushalien <agrawaltushar63@gmail.com>
2018-04-07 06:22:02 +0530

    Added IPFS compatablity

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,25 @@
 import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
+import bs58 from 'bs58'
+
 
 import freelancer_artifacts from '../../build/contracts/Freelancer.json'
 
 var Freelancer = contract(freelancer_artifacts);
 //console.log(Freelancer);
+
+
+getBytes32FromIpfsHash(ipfsListing) {
+  return "0x"+bs58.decode(ipfsListing).slice(2).toString('hex')
+}
+
+getIpfsHashFromBytes32(bytes32Hex) {
+  const hashHex = "1220" + bytes32Hex.slice(2)
+  const hashBytes = Buffer.from(hashHex, 'hex');
+  const hashStr = bs58.encode(hashBytes)
+  return hashStr
+}
+
 
 
 
